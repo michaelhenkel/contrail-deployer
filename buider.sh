9780fabaed0028156bccfc6652b76e2c5a8ed933
Michael Henkel <mhenkel@juniper.net>
2018-05-05 16:22:36 -0700

    new binaries

--- buider.sh
+++ buider.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 case $1 in
 	mac)
-		GOOS=darwin GOARCH=amd64 go build -o contrail-deployer contrail-deployer.go;;
+		GOOS=darwin GOARCH=amd64 go build -o contrail-deployer_darwin contrail-deployer.go;;
 	linux)
-		GOOS=linux GOARCH=amd64 GOARM=7 go build -o contrail-deployer contrail-deployer.go;;
+		GOOS=linux GOARCH=amd64 GOARM=7 go build -o contrail-deployer_linux contrail-deployer.go;;
 	win)
 		GOOS=windows GOARCH=386 go build -o contrail-deployer.exe contrail-deployer.go;;
 	*)
